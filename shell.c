55c8eb251ed2d998adb5d8558894cb8c9d821ee9
zhoudazhi <dazhi527@hotmail.com>
2023-03-19 18:11:06 +0800

    修改了管道部分。增加了readme

--- shell.c
+++ shell.c
@@ -8,6 +8,8 @@
 #define MAX_BUFFER_SIZE 1024
 #define MAX_ARGS 128
 
+int find_pipe(char **args);
+void execute_pipeline(char **args, int in, int out, int err);
 void handle_redirection(char *args[], int *p_in, int *p_out, int *p_append, int *p_err);
 void execute_command(char *args[], const int *p_in, const int *p_out, const int *p_append, const int *p_err);
 void print_help();
@@ -37,7 +39,7 @@ int main(int argc, char *argv[]) {
         if (fgets(buffer, MAX_BUFFER_SIZE, stdin) == NULL) { // EOF
             break;
         }
-        if (strcmp(buffer, "exit") == 0) { // exit
+        if (strcmp(buffer, "exit\n") == 0) { // exit
             printf("Bye!:)\n");
             break;
         }
@@ -51,7 +53,7 @@ int main(int argc, char *argv[]) {
         if (args[0] == NULL) { // empty command
             continue;
         }
-        execute_command(args, &in, &out, &append, &err); // execute command
+        execute_pipeline(args, in, out, err);
     }
     return 0;
 }
@@ -68,6 +70,50 @@ void parse_command(char *line, char **args) {
     args[argc] = NULL;
 }
 
+int find_pipe(char **args) {
+    int i = 0;
+    while (args[i] != NULL) {
+        if (strcmp(args[i], "|") == 0) {
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
+void execute_pipeline(char **args, int in, int out, int err) {
+    int pipe_loc = find_pipe(args);
+
+    if (pipe_loc == -1) {
+        // 没有管道，直接执行命令
+        int append = 0;
+        execute_command(args, &in, &out, &append, &err);
+    } else {
+        args[pipe_loc] = NULL; // split args at pipe_loc
+        // Create a pipe
+        int pipe_fd[2];
+        if (pipe(pipe_fd) < 0) {
+            perror("pipe");
+            exit(1);
+        }
+
+        pid_t pid = fork(); // Create child process for the left-side command
+        if (pid < 0) {
+            perror("fork");
+            exit(1);
+        } else if (pid == 0) { // Child
+            close(pipe_fd[0]); // Close pipe_out
+            execute_pipeline(args, in, pipe_fd[1], err);
+            close(pipe_fd[1]); // Close pipe_in
+            exit(0);
+        } else { // Parent
+            close(pipe_fd[1]); // Close pipe_in
+            execute_pipeline(args + pipe_loc + 1, pipe_fd[0], out, err);
+            close(pipe_fd[0]); // Close pipe_out
+            waitpid(pid, NULL, 0); // Wait for child process
+        }
+    }
+}
 void handle_redirection(char *args[], int *p_in, int *p_out, int *p_append, int *p_err) {
     int in = *p_in;
     int out = *p_out;
@@ -108,32 +154,11 @@ void handle_redirection(char *args[], int *p_in, int *p_out, int *p_append, int 
             }
             args[argc] = NULL;
             argc++;
-        } else if (strcmp(args[argc], "|") == 0) { // |
-            int pipe_fd[2]; // pipe_fd[0]: out, pipe_fd[1]: in
-            if (pipe(pipe_fd) < 0) { // create pipe
-                perror("pipe");
-                exit(1);
-            }
-            pid_t pid = fork(); // create child process(left command)
-            // pipe: child_out -> parent_in
-            if (pid < 0) {
-                perror("fork");
-                exit(1);
-            } else if (pid == 0) { // child
-                close(pipe_fd[0]); // close pipe_out
-                handle_redirection(&args[argc], p_in, &pipe_fd[1], p_append, &err); // handle redirection
-                execute_command(&args[argc], p_in, &pipe_fd[1], p_append, &err); // execute command: p_in -> pipe_out --pipe--> parent_in
-                exit(0);
-            } else { // parent
-                close(pipe_fd[1]);// close pipe_in
-                in = pipe_fd[0]; // child_out --pipe--> parent_in
-                waitpid(pid, NULL, 0); // wait for child process
-            }
         } else {
             argc++; // next argument
         }
     }
-    // update in, out, append, err
+// update in, out, append, err
     *p_in = in;
     *p_out = out;
     *p_append = append;
@@ -145,54 +170,56 @@ void execute_command(char *args[], const int *p_in, const int *p_out, const int 
     int out = *p_out;
     int append = *p_append;
     int err = *p_err;
-    int background = 0; // is background process
-    int argc = 0; // argument count
-    while (args[argc] != NULL) { // check all arguments
-        if (strcmp(args[argc], "&") == 0) { // & (background process)
-            args[argc] = NULL;
-            background = 1;
-            break;
-        } else {
-            argc++;
-        }
-    }
     pid_t pid = fork(); // create child process
     if (pid < 0) {
         perror("fork");
         exit(1);
-    } else if (pid == 0) { // child
-        if (in != STDIN_FILENO) { // redirect in
+    } else if (pid== 0) { // Child
+        if (in != STDIN_FILENO) { // Redirect input
             dup2(in, STDIN_FILENO);
             close(in);
         }
-        if (out != STDOUT_FILENO) { // redirect out
+        if (out != STDOUT_FILENO) { // Redirect output
             dup2(out, STDOUT_FILENO);
             close(out);
         }
-        if (err != STDERR_FILENO) { // redirect err
+        if (err != STDERR_FILENO) { // Redirect error
             dup2(err, STDERR_FILENO);
             close(err);
         }
-        if (append) { // append
+        if (append) { // Append
             dup2(out, STDERR_FILENO);
         }
-        if (execvp(args[0], args) < 0) { // execute command
+        if (execvp(args[0], args) < 0) { // Execute command
             perror("execvp");
             exit(1);
         }
-    } else {
-        if (in != STDIN_FILENO) { // close in
+    } else { // Parent
+        if (in != STDIN_FILENO) { // Close input
             close(in);
         }
-        if (out != STDOUT_FILENO) {
+        if (out != STDOUT_FILENO) { // Close output
             close(out);
         }
-        if (err != STDERR_FILENO) {
+        if (err != STDERR_FILENO) { // Close error
             close(err);
         }
-        if (!background) {
-            waitpid(pid, NULL, 0);
-        }
+        waitpid(pid, NULL, 0); // Wait for child process
     }
 }
+
+void print_help() {
+    printf("Usage: zzsh [OPTION] [FILE] [ARG] [<>|] ...\n");
+    printf("A simple shell that supports basic file redirection and piping.\n");
+    printf("\nOptions:\n");
+    printf(" -h\tPrint this help information.\n");
+    printf("\nRedirection:\n");
+    printf(" <\tRead input from a file.\n");
+    printf(" >\tWrite output to a file (overwrite).\n");
+    printf(" >>\tWrite output to a file (append).\n");
+    printf(" 2>\tWrite error output to a file.\n");
+    printf("\nPiping:\n");
+    printf(" |\tPipe the output of one command as input to another command.\n");
+}
+
 
