989f67e2669df0db1776a4fce89e792ef5504141
zhoudazhi <dazhi527@hotmail.com>
2023-03-20 16:14:06 +0800

    添加了n> n>> 的功能

--- shell.c
+++ shell.c
@@ -1,9 +1,10 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
-#include <fcntl.h>
+#include <unistd.h>
+#include <regex.h>
 
 #define MAX_BUFFER_SIZE 1024
 #define MAX_ARGS 128
@@ -15,34 +16,38 @@ void execute_command(char *args[], const int *p_in, const int *p_out, const int 
 void print_help();
 void parse_command(char *line, char **args);
 
+int orig_stdin;
+int orig_stdout;
+int orig_stderr;
+
 int main() {
-    // Read command buffer
     char buffer[MAX_BUFFER_SIZE];
-    int in = STDIN_FILENO; // input
-    int out = STDOUT_FILENO; // output
-    int append = 0; // 0: not append, 1: append
-    int err = STDERR_FILENO; // error
+    int in = STDIN_FILENO, out = STDOUT_FILENO, err = STDERR_FILENO, append = 0;
+    orig_stdin = dup(STDIN_FILENO);
+    orig_stdout = dup(STDOUT_FILENO);
+    orig_stderr = dup(STDERR_FILENO);
+
     while (1) {
         printf("zzsh>> ");
-        if (fgets(buffer, MAX_BUFFER_SIZE, stdin) == NULL) { // EOF
+        if (fgets(buffer, MAX_BUFFER_SIZE, stdin) == NULL) {
             break;
         }
-        if (strcmp(buffer, "exit\n") == 0) { // exit
+        if (strcmp(buffer, "exit\n") == 0) {
             printf("Bye!:)\n");
             break;
         }
-        if (strcmp(buffer, "help\n") == 0) { // help
+        if (strcmp(buffer, "help\n") == 0) {
             print_help();
             continue;
         }
         int buffer_len = strlen(buffer);
-        if (buffer_len > 0 && buffer[buffer_len - 1] == '\n') { // remove '\n'
+        if (buffer_len > 0 && buffer[buffer_len - 1] == '\n') {
             buffer[buffer_len - 1] = '\0';
         }
         char *args[MAX_ARGS];
-        parse_command(buffer, args); // e.g: ls -l | less -> args = ["ls", "-l", "|", "less", NULL]
-        handle_redirection(args, &in, &out, &append, &err); // handle redirection
-        if (args[0] == NULL) { // empty command
+        parse_command(buffer, args);
+        handle_redirection(args, &in, &out, &append, &err);
+        if (args[0] == NULL) {
             continue;
         }
         execute_pipeline(args, in, out, err);
@@ -52,8 +57,7 @@ int main() {
 
 void parse_command(char *line, char **args) {
     int argc = 0;
-    char *token;
-    char *save_ptr;
+    char *token, *save_ptr;
     token = strtok_r(line, " ", &save_ptr);
     while (token != NULL) {
         args[argc++] = token;
@@ -77,43 +81,41 @@ void execute_pipeline(char **args, int in, int out, int err) {
     int pipe_loc = find_pipe(args);
 
     if (pipe_loc == -1) {
-        // 没有管道，直接执行命令
         int append = 0;
         execute_command(args, &in, &out, &append, &err);
     } else {
-        args[pipe_loc] = NULL; // split args at pipe_loc
-        // Create a pipe
+        args[pipe_loc] = NULL;
         int pipe_fd[2];
         if (pipe(pipe_fd) < 0) {
             perror("pipe");
             exit(1);
         }
-
-        pid_t pid = fork(); // Create child process for the left-side command
+        pid_t pid = fork();
         if (pid < 0) {
             perror("fork");
             exit(1);
-        } else if (pid == 0) { // Child
-            close(pipe_fd[0]); // Close pipe_out
+        } else if (pid == 0) {
+            close(pipe_fd[0]);
             execute_pipeline(args, in, pipe_fd[1], err);
-            close(pipe_fd[1]); // Close pipe_in
+            close(pipe_fd[1]);
             exit(0);
-        } else { // Parent
-            close(pipe_fd[1]); // Close pipe_in
+        } else {
+            close(pipe_fd[1]);
             execute_pipeline(args + pipe_loc + 1, pipe_fd[0], out, err);
-            close(pipe_fd[0]); // Close pipe_out
-            waitpid(pid, NULL, 0); // Wait for child process
+            close(pipe_fd[0]);
+            waitpid(pid, NULL, 0);
         }
     }
+    if (in != STDIN_FILENO) dup2(orig_stdin, STDIN_FILENO);
+    if (out != STDOUT_FILENO) dup2(orig_stdout, STDOUT_FILENO);
+    if (err != STDERR_FILENO) dup2(orig_stderr, STDERR_FILENO);
 }
+
 void handle_redirection(char *args[], int *p_in, int *p_out, int *p_append, int *p_err) {
-    int in = *p_in;
-    int out = *p_out;
-    int append = *p_append;
-    int err = *p_err;
-    int argc = 0; // argument count
-    while (args[argc] != NULL) { // check all arguments ?(<>|)
-        if (strcmp(args[argc], "<") == 0) { // <
+    int in = *p_in, out = *p_out, append = *p_append, err = *p_err;
+    int argc = 0, n, n_append;
+    while (args[argc] != NULL) {
+        if (strcmp(args[argc], "<") == 0) {
             in = open(args[argc + 1], O_RDONLY);
             if (in < 0) {
                 perror("open");
@@ -121,15 +123,15 @@ void handle_redirection(char *args[], int *p_in, int *p_out, int *p_append, int 
             }
             args[argc] = NULL;
             argc++;
-        } else if (strcmp(args[argc], ">") == 0) { // >
-            out = open(args[argc + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // rw-r--r--
+        } else if (strcmp(args[argc], ">") == 0) {
+            out = open(args[argc + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
             if (out < 0) {
                 perror("open");
                 exit(1);
             }
             args[argc] = NULL;
             argc++;
-        } else if (strcmp(args[argc], ">>") == 0) { // >>
+        } else if (strcmp(args[argc], ">>") == 0) {
             out = open(args[argc + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
             if (out < 0) {
                 perror("open");
@@ -138,19 +140,40 @@ void handle_redirection(char *args[], int *p_in, int *p_out, int *p_append, int 
             args[argc] = NULL;
             argc++;
             append = 1;
-        } else if (strcmp(args[argc], "2>") == 0) { // 2>
-            err = open(args[argc + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (err < 0) {
-                perror("open");
-                exit(1);
-            }
-            args[argc] = NULL;
-            argc++;
         } else {
-            argc++; // next argument
+            regex_t regex1, regex2;
+            regcomp(&regex1, "^[0-9]+>$", REG_EXTENDED);
+            regcomp(&regex2, "^[0-9]+>>$", REG_EXTENDED);
+            if (regexec(&regex1, args[argc], 0, NULL, 0) == 0) { // n> file
+                sscanf(args[argc], "%d>", &n);
+                int fd = open(args[argc + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if (dup2(fd, n) < 0) {
+                    perror("dup2");
+                    exit(1);
+                }
+                close(fd); // Close the file descriptor after dup2
+                args[argc] = NULL;
+                args[argc + 1] = NULL;
+                argc += 2;
+            } else if (regexec(&regex2, args[argc], 0, NULL, 0) == 0) { // n>> file
+                sscanf(args[argc], "%d>>", &n);
+                int fd = open(args[argc + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+                if (dup2(fd, n) < 0) {
+                    perror("dup2");
+                    exit(1);
+                }
+                close(fd); // Close the file descriptor after dup2
+                args[argc] = NULL;
+                args[argc + 1] = NULL;
+                argc += 2;
+            }
+            else {
+                argc++; // next argument
+            }
+            regfree(&regex1);
+            regfree(&regex2);
         }
     }
-// update in, out, append, err
     *p_in = in;
     *p_out = out;
     *p_append = append;
@@ -166,7 +189,7 @@ void execute_command(char *args[], const int *p_in, const int *p_out, const int 
     if (pid < 0) {
         perror("fork");
         exit(1);
-    } else if (pid== 0) { // Child
+    } else if (pid == 0) { // Child
         if (in != STDIN_FILENO) { // Redirect input
             dup2(in, STDIN_FILENO);
             close(in);
@@ -196,20 +219,29 @@ void execute_command(char *args[], const int *p_in, const int *p_out, const int 
         if (err != STDERR_FILENO) { // Close error
             close(err);
         }
+
+        // 恢复原始的文件描述符
+        dup2(orig_stdin, STDIN_FILENO);
+        dup2(orig_stdout, STDOUT_FILENO);
+        dup2(orig_stderr, STDERR_FILENO);
+
         waitpid(pid, NULL, 0); // Wait for child process
     }
 }
 
+
+
 void print_help() {
-    printf("Usage: zzsh [OPTION] [FILE] [ARG] [<>|] ...\n");
-    printf("A simple shell that supports basic file redirection and piping.\n");
-    printf("\nRedirection:\n");
-    printf(" <\tRead input from a file.\n");
-    printf(" >\tWrite output to a file (overwrite).\n");
-    printf(" >>\tWrite output to a file (append).\n");
-    printf(" 2>\tWrite error output to a file.\n");
-    printf("\nPiping:\n");
-    printf(" |\tPipe the output of one command as input to another command.\n");
+    printf("zzsh - A simple shell implementation\n");
+    printf("Usage: command [arguments]\n");
+    printf("Built-in commands:\n");
+    printf("  help          - Display this help message\n");
+    printf("  exit          - Exit the shell\n");
+    printf("Redirections and pipes:\n");
+    printf("  command > file   - Redirect command output to file\n");
+    printf("  command >> file  - Append command output to file\n");
+    printf("  command < file   - Redirect input from file to command\n");
+    printf("  command n> file  - Redirect command output to file descriptor n\n");
+    printf("  command n>> file - Append command output to file descriptor n\n");
+    printf("  command1 | command2 - Pipe output of command1 to input of command2\n");
 }
-
-
